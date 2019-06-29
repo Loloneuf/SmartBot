c41af874f92189fa4f952c594113336a0b96a2dc
Loloneuf <lorisoumbiche@gmail.com>
2019-06-29 17:41:52 +0200

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,17 @@
+let listbox=['boxe1','boxe2','boxe3']
+let listcolor=['rgb(255, 0, 0)','rgb(0, 255, 0)','rgb(0, 0, 255)',"rgb(255,255,255)"]
+function setcolor(list,listc){
+ for (j=0;j<3;j++){
+ document.getElementById(list[j]).style.backgroundColor = listc[3]
+ }
+}
+function clignotement(list,listc){
+for (i=0;i<3;i++){
+if (document.getElementById(list[i]).style.backgroundColor==listc[i]) {
+	document.getElementById(list[i]).style.backgroundColor = listc[3]; 
+}
+else {document.getElementById(list[i]).style.backgroundColor = listc[i]
+}}}
+setcolor(listbox,listcolor)
+clignotement(listbox,listcolor)
+setInterval("clignotement(listbox,listcolor)", 1000); 
