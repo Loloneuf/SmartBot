c2759fedc051edbba52cb17edc88c02f9821812b
Loloneuf <lorisoumbiche@gmail.com>
2019-07-02 02:19:52 +0200

    Create Bouton.js

---
+++ Bouton.js
@@ -0,0 +1,18 @@
+let Submit=document.querySelector('#submit');
+let listselect = [document.querySelector('#Color1'),document.querySelector('#Color2'),document.querySelector('#Color3')]
+let listbox2=['boxe1','boxe2','boxe3']
+let listcolor2=['rgb(255, 0, 0)','rgb(0, 255, 0)','rgb(0, 0, 255)',"rgb(255, 255, 255)" , "rgb(0, 0, 0)"]
+let listname=['Red', 'Green', 'Blue', 'White', 'Black']
+let Ptdr = []
+function active() {
+	for (g=0;g<3;g++){
+		for(h=0;h<5;h++){
+			if (listselect[g].value === listname[h]) {
+				document.getElementById(listbox2[g]).style.backgroundColor= listcolor2[h];
+				Ptdr.push(listcolor2[h])
+				}
+		}
+	}
+}
+Submit.addEventListener('click', active)
+	
