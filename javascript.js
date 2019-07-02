42b47ee9513a6c671faf2a78a363424363844afa
Loloneuf <43267724+Loloneuf@users.noreply.github.com>
2019-07-02 02:19:15 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 let listbox=['boxe1','boxe2','boxe3']
 let listcolor=['rgb(255, 0, 0)','rgb(0, 255, 0)','rgb(0, 0, 255)',"rgb(255,255,255)"]
+
+
 function setcolor(list,listc){
  for (j=0;j<3;j++){
  document.getElementById(list[j]).style.backgroundColor = listc[3]
@@ -7,11 +9,13 @@ function setcolor(list,listc){
 }
 function clignotement(list,listc){
 for (i=0;i<3;i++){
-if (document.getElementById(list[i]).style.backgroundColor==listc[i]) {
+if (document.getElementById(list[i]).style.backgroundColor== Newcolor[i]) {
 	document.getElementById(list[i]).style.backgroundColor = listc[3]; 
 }
-else {document.getElementById(list[i]).style.backgroundColor = listc[i]
+else {document.getElementById(list[i]).style.backgroundColor = Newcolor[i]
 }}}
 setcolor(listbox,listcolor)
 clignotement(listbox,listcolor)
 setInterval("clignotement(listbox,listcolor)", 1000); 
+
+
