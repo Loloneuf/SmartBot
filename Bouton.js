334dd0e4de7be061118c6044150d1fb8ae5c6dca
Loloneuf <43267724+Loloneuf@users.noreply.github.com>
2019-09-17 14:55:37 +0200

    Update Bouton.js

--- Bouton.js
+++ Bouton.js
@@ -3,16 +3,16 @@ let listselect = [document.querySelector('#Color1'),document.querySelector('#Col
 let listbox2=['boxe1','boxe2','boxe3']
 let listcolor2=['rgb(255, 0, 0)','rgb(0, 255, 0)','rgb(0, 0, 255)',"rgb(255, 255, 255)","rgb(0, 0, 0)","rgb(255, 255, 0)","rgb(0, 255, 255)","rgb(255, 0, 255)"]
 let listname=['Red', 'Green', 'Blue', 'White', 'Black', 'Yellow', 'Cyan', 'Magenta']
-let Ptdr = []
+let List_for_color_change = []
 function active() {
-	Ptdr.shift()
-	Ptdr.shift()
-	Ptdr.shift()
+	List_for_color_change.shift()
+	List_for_color_change.shift()
+	List_for_color_change.shift()
 	for (g=0;g<3;g++){
 		for(h=0;h<8;h++){
 			if (listselect[g].value === listname[h]) {
 				document.getElementById(listbox2[g]).style.backgroundColor= listcolor2[h];
-				Ptdr.push(listcolor2[h]) 
+				List_for_color_change.push(listcolor2[h]) 
 			}
 			
 			
@@ -20,7 +20,7 @@ function active() {
 		
 	if(listname.indexOf(listselect[g].value) === -1) {
 				document.getElementById(listbox2[g]).style.backgroundColor= "rgb(255, 255, 255)"
-			Ptdr.push("rgb(255, 255, 255)") }
+			List_for_color_change.push("rgb(255, 255, 255)") }
 	}
 }
 Submit.addEventListener('click', active)
